c0ffd254c8c97be39cc989eff8fe455049be12e5
teciu78 <w.teclaw@gmail.com>
2023-07-10 06:01:42 +0200

    Zadanie domowe: Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool isFemale = true;
+int age = 33;
+
+if (isFemale)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine($"{name}, lat {age}");
+    }
+
+
+} 
+else if (!isFemale)
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    } 
+    else
+    {
+        Console.WriteLine($"{name}, lat {age}");
+    }
+}
